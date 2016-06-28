a8a46630d497c6e7802fdf29a0f47e1aa21b9584
DavidNovo <david.novogrodsky@gmail.com>
2016-06-27 21:10:38 -0500

    first draft for simple app

---
+++ src/test/java/net/novogrodsky/AppTest.java
@@ -0,0 +1,28 @@
+package net.novogrodsky;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by davidnovogrodsky_wrk on 6/27/16.
+ */
+public class AppTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testMain() throws Exception {
+
+    }
+}
